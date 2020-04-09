0f3185fe1e6f99f482d48490f0ea8b0b48baaa30
Dagan3D <GameWorldDan@gmail.com>
2020-04-09 14:23:59 +0300

    Class User join

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,53 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+data class User (
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    var lastVisit : Date? = Date(),
+    var isOnline : Boolean = false
+) {
+
+    constructor(id: String, firstName: String?, lastName: String?): this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+    constructor(id: String): this(id, "Dan", "Kanor")
+
+    fun printMe() = println("""
+        firstName: $firstName
+        lastName: $lastName
+        avatar: $avatar
+        rating: $rating
+        respect: $respect
+        lastVisit: $lastVisit
+        isOnline: $isOnline        
+    """.trimIndent())
+
+    companion object Factory {
+        private var last_id = -1
+
+        fun makeUser (fullNane: String?): User{
+            last_id++
+
+            val parts : List<String>? = fullNane?.trimIndent()?.split(" ")
+
+            val lastName = parts?.getOrNull(0)
+            val firstName = parts?.getOrNull(1)
+
+            return when {
+                lastName == null    -> User(id = "$last_id")
+                firstName == null   -> User(id = "$last_id")
+                else ->  User(id = "$last_id", firstName = firstName, lastName = lastName)
+            }
+
+        }
+    }
+}
